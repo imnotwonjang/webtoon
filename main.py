707c4bf763ff0798eec4ae9a03d7dfad5e651a7f
조원장 <wonjang@jowonjang-ui-noteubug.local>
2023-01-28 21:31:31 +0900

    ..

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
+import pandas as pd
 
+
+
+app = FastAPI()
 
 @app.get("/")
 async def root():
+
+    URL = 'https://comic.naver.com/webtoon/weekday.nhn'
+
+    html = requests.get(URL).text # html 문서 전체를 긁어서 출력해줌, .text는 태그 제외하고 text만 출력되게 함
+    soup = BeautifulSoup(html, 'html.parser')
     return {"message": "Hello World"}
